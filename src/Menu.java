8e3627db258ba43229f553ea82e6dfc86ba5668b
cristian-simba <davidsimba200@gmail.com>
2023-05-30 09:50:34 -0500

    Tercer Avance

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,10 @@ public class Menu {
         Peliculas Pelicula1= new Peliculas("El pianista","Drama","Roman Polanski",2002);
         Peliculas Pelicula2= new Peliculas("50 sombras de grey","Romantica","Sam Taylor-Wood",2015);
         Peliculas Pelicula3= new Peliculas("Spider-Man: un nuevo universo","Animacion","Peter Ramsey",2018);
-        Peliculas Pelicila4=new Peliculas("barbie life in the dreamhouse","Animacion","David Wiebe",2018);
+        Peliculas Pelicula4= new Peliculas("barbie life in the dreamhouse","Animacion","David Wiebe",2018);
+        Peliculas Pelicula5 = new Peliculas("Spiderman","Acción","Sam Raimi",2022 );
+        Peliculas Pëlicula6 = new Peliculas("Wall-e", "Animacion"," Andrew Stanton", 2008);
+        Peliculas Pelicula7 = new Peliculas("Rapidos y furiosos X", "Acción", "Louis Leterrier",2023);
+        Peliculas Pelicula8 = new Peliculas("Capitan America y el soldado del invierno", "Acción","Anthony Russo",2014);
     }
 }
