4dca973250a3b61ca38ffcc3eb7a5f71cebbdf09
Ruizerick26 <ruizerick2525@gmail.com>
2023-05-31 09:15:34 -0500

    Version final

--- src/Menu.java
+++ src/Menu.java
@@ -5,8 +5,38 @@ public class Menu {
         Peliculas Pelicula3= new Peliculas("Spider-Man: un nuevo universo","Animacion","Peter Ramsey",2018);
         Peliculas Pelicula4= new Peliculas("barbie life in the dreamhouse","Animacion","David Wiebe",2018);
         Peliculas Pelicula5 = new Peliculas("Spiderman","Acción","Sam Raimi",2022 );
-        Peliculas Pëlicula6 = new Peliculas("Wall-e", "Animacion"," Andrew Stanton", 2008);
+        Peliculas Pelicula6 = new Peliculas("Wall-e", "Animacion"," Andrew Stanton", 2008);
         Peliculas Pelicula7 = new Peliculas("Rapidos y furiosos X", "Acción", "Louis Leterrier",2023);
-        Peliculas Pelicula8 = new Peliculas("Capitan America y el soldado del invierno", "Acción","Anthony Russo",2014);
+        Peliculas Pelicula8 = new Peliculas("Batman El caballero de la noche", "Acción","Christopher Nolan",2008);
+        Peliculas Pelicula9 = new Peliculas("Ironman","Acción","Jon Favreau",2008 );
+        Peliculas Pelicula10 = new Peliculas("IT", "Terror","Andrés Muschietti", 2017);
+        Peliculas Pelicula11 = new Peliculas("The thing", "Terror", "John Carpenter",1982);
+        Peliculas Pelicula12 = new Peliculas("The Blair witch Proyect", "Terror","Eduardo Sanchez , Daniel Myrick",1999);
+        System.out.println("\nPelicula 1:");
+        Pelicula1.imprimir_Datos();
+        System.out.println("\nPelicula 2:");
+        Pelicula2.imprimir_Datos();
+        System.out.println("\nPelicula 3:");
+        Pelicula3.imprimir_Datos();
+        System.out.println("\nPelicula 4:");
+        Pelicula4.imprimir_Datos();
+        System.out.println("\nPelicula 5:");
+        Pelicula5.imprimir_Datos();
+        System.out.println("\nPelicula 6:");
+        Pelicula6.imprimir_Datos();
+        System.out.println("\nPelicula 7:");
+        Pelicula7.imprimir_Datos();
+        System.out.println("\nPelicula 8:");
+        Pelicula8.imprimir_Datos();
+        System.out.println("\nPelicula 9:");
+        Pelicula9.imprimir_Datos();
+        System.out.println("\nPelicula 10:");
+        Pelicula10.imprimir_Datos();
+        System.out.println("\nPelicula 11:");
+        Pelicula11.imprimir_Datos();
+        System.out.println("\nPelicula 12:");
+        Pelicula12.imprimir_Datos();
+
+
     }
 }
